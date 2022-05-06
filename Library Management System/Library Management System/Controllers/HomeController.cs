02045fa4f4afce706b2f5a4c6c34953ceb95044b
Pixel-Man360 <darkblade6952@gmail.com>
2022-05-06 23:18:36 +0600

    Initialised

---
+++ Library Management System/Library Management System/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Library_Management_System.Models;
+
+namespace Library_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("LibraryManagementSystemHome/index")]
+        public IActionResult Index()
+        {
+           // throw new Exception();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
